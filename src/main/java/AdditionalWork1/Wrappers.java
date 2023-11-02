d03890bdd2667c71754d842f3cb1eb34e402a47f
Tapka3000 <circle.table@yandex.ru>
2023-11-02 11:47:21 +0300

    Commit

--- src/main/java/AdditionalWork1/Wrappers.java
+++ src/main/java/AdditionalWork1/Wrappers.java
@@ -2,14 +2,14 @@ package AdditionalWork1;
 
 public class Wrappers {
     public static void main(String[] args) {
-        Integer a = new Integer(55);
-        Byte a1 = new Byte((byte) 124);
-        Short b = new Short((short) 275185);
-        Long d = new Long((long) 1000000000000000000l);
-        Float e = new Float((float) 154.45f);
-        Double f = new Double((double) 1550.54d);
-        Boolean j = new Boolean(true);
-        Character h = new Character('f');
+        Integer I = 55;
+        Byte B = 124;
+        Short S = 23;
+        Long L = 1000000000000000000l;
+        Float F = 154.45f;
+        Double D = 1550.54d;
+        Boolean B1 = true;
+        Character Ch = 'f';
 
     }
 }
